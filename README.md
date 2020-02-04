84b71711964ecad57e48291b4f64b0a8f64fa829
Ola Dominas <o.dominas@gmail.com>
2020-02-04 17:34:14 +0100

    wip

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# DiscoveryArduino
+# Discover Arduino
 
-Display room temperature on a LCD screen.
+A small Arduino project which shows current room temperature on an LCD screen.
+
+## Used components
+
+### 1. Sensor TMP36
+
+The project uses a standard analog TMP36 sensor to read the ambient temperature. The datasheet can be found here <https://bit.ly/2SacHcB>.
+
+#### How to calculate the temperature
+
+The sensor outputs a 10-bit analog value ranging from 0 to 1023, which directly corresponds to a voltage. We can find the ratio of this correlation $r$ by dividing the analog output by a number of possible values, which in our case, is 1024. Then, as we are usin
+
 
 ## Notes
 - code
--- project.ino
+++ project.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 
+// 
 int tempSensorPin = A0;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //pins used by screen
@@ -8,24 +9,26 @@ void setup()
 {
   Serial.begin(9600);
 
+  // initilize the interface to the LCD screen with 16 columns and 2 rows 
   lcd.begin(16, 2); 
 
   lcd.print("Room temperature: ");
-
-
 }
 
 void loop()
 {
   int tempSensorValue = analogRead(tempSensorPin);
 
-  // https://www.arduino.cc/en/uploads/Main/TemperatureSensor.pdf figure 6
+  // convert reading from temperature to voltage, formula explained in README 
   float voltage = (tempSensorValue / 1024.0) * 5.0;
-  float temperature = (voltage - 0.5) * 100;
+
+// calculate room temperature
+  float roomTemperature = (voltage - 0.5) * 100;
   
+  // set screen cursor in the first column, the second row
   lcd.setCursor(0, 1);
 
-  lcd.print(String(temperature) + String((char)223) +"C");
+  lcd.print(String(roomTemperature) + String((char)223) +"C");
   
   delay(1000);
 }
